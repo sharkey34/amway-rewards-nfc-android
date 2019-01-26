d0433fc1d8c14a4c0d66a182b19749538b871ff4
Eric Sharkey <eisharkey@student.fullsail.edu>
2019-01-25 23:40:00 -0500

    ADD - scavenger item layout

--- app/src/main/java/com/example/ericsharkey/amwayrewards/fragments/NFCFragment.java
+++ app/src/main/java/com/example/ericsharkey/amwayrewards/fragments/NFCFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -42,7 +41,7 @@ public class NFCFragment extends Fragment {
             String title = bundle.getString(Const.EXTRA_TITLE);
             String points = bundle.getString(Const.EXTRA_POINTS);
 
-            TextView titleTv = getActivity().findViewById(R.id.title_text);
+            TextView titleTv = getActivity().findViewById(R.id.scavenger_title);
             TextView pointsTv = getActivity().findViewById(R.id.points_text);
 
             titleTv.setText(title);
